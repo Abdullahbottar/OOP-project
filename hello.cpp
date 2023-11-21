3acb090b24ab15ce3a74c05e87af9a0dd5edc83a
abdullahbottar <abdullahbottar921@gmail.com>
2023-11-22 01:08:31 +0500

    second commit

---
+++ hello.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   cout<<"hello world"<<endl;
+   for(int i=0; i<5; i++)
+   {
+    cout<<i+5<<" ";
+   }
+   cout<<endl;
+   return 0;
+}
+
+
+
