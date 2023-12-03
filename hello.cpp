1c8973c9c8c2538137803b7b57b5c5c13ac3be12
abdullahbottar <abdullahbottar921@gmail.com>
2023-12-04 01:36:21 +0500

    2nd commit

--- hello.cpp
+++ hello.cpp
@@ -2,31 +2,53 @@
 #include <fstream>
 #include <string>
 using namespace std;
+
+class Course;
 class Student
 {
   string name;
   string rollnumber;
   int age;
   string contactno;
+  Course * courses;
+  int marks;    //to be determined
   public:
 };
 class Course
 {
   string code;
+  string Instructor;
   string name;
   int credits;
-  int capacity;
+  int capacity;         //to be determined
   Student *students;
   public:
   
+};
+class Validator
+{
+  public:
+        Validator(){ }
+        
+        int input(int choice,int ll,int ul)
+        {
+          cin>>choice;
+         while(choice<ll || choice >ul)
+          {
+           cout<<"ENTER CORRECT CHOICE:";
+           cin>>choice;
+          }
+          return choice;
+        }
+
 };
 class System
 {
    public:
    System(){ }
-   void choice()
+   void Menu()
    {
-  int choice1;
+  int choice=0;
   cout<< "1- Enroll a student.\n";
   cout<< "2- Course Registration.\n";
   cout<< "3- Attendence.\n";
@@ -35,18 +57,55 @@ class System
   cout<< "6- Exit\n";
   cout<<"\n\n";
   cout<<"ENTER YOUR CHOICE:";
-  cin>>choice1;
-  while(choice1<1 || choice1 >6)
+  Validator obj;
+  choice=obj.input(choice,1,6);
+  if (choice ==1)
   {
-    cout<<"ENTER CORRECT CHOICE:";
-    cin>>choice1;
+    System::enroll();
+  }
+  if(choice==2)
+  {
+
+  }
+  if(choice==3)
+  {
+
+  }
+  if(choice ==4)
+  {
+
+  }
+  if(choice ==5)
+  {
+
+  }
+  if(choice ==6)
+  {
+
   }
    }
+  void enroll()
+  {
+    string name;
+    string contactno;
+    int age;
+    string rollno;
+    int marks;
+    cout<<"Enter Student Name:";
+    cout<<"Enter Student Age:";
+    cout<<"Enter Student Roll number:";
+    cout<<"Enter Student:";
+  }
    ~System(){ }
 };
+class Filehandler
+{
+};
+
 int main()
 {
-
+  System obj;
+  obj.Menu();
    return 0;
 }
 
