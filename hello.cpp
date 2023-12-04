7fe70a2bd7a9244781b3db41ee46354680dd8020
abdullahbottar <abdullahbottar921@gmail.com>
2023-12-05 01:52:13 +0500

    4th commit

--- hello.cpp
+++ hello.cpp
@@ -42,7 +42,7 @@ class Validator:public Student
         }
         void enroll()
         {
-              string name;
+              string name="0";
               string contactno;
               int age;
               string rollno;
@@ -66,73 +66,142 @@ class System:public Validator
    System(){ }
    void Menu()
    {
-  int choice=0;
-  cout<< "1- Enroll a student.\n";
-  cout<< "2- Course Registration.\n";
-  cout<< "3- Attendence.\n";
-  cout<< "4- Marks.\n";
-  cout<< "5- Course withdraw.\n";
-  cout<< "6- Exit\n";
-  cout<<"\n\n";
-  cout<<"ENTER YOUR CHOICE:";
-  Validator::input(choice,1,6);
-  if (choice ==1)
-  {
-    System::enroll();
-  }
-  if(choice==2)
-  {
-
-  }
-  if(choice==3)
-  {
-
-  }
-  if(choice ==4)
-  {
-
-  }
-  if(choice ==5)
-  {
-
-  }
-  if(choice ==6)
-  {
-   
-  }
+     int choice=0;
+     cout<< "1- Enroll a student.\n";
+     cout<< "2- Course Registration.\n";
+     cout<< "3- Attendence.\n";
+     cout<< "4- Marks.\n";
+     cout<< "5- Course withdraw.\n";
+     cout<< "6- Exit\n";
+     cout<<"\n\n";
+     cout<<"ENTER YOUR CHOICE:";
+     Validator::input(choice,1,6);
+     if (choice ==1)
+      {
+        System::enroll();
+      }
+     if(choice==2)
+      {
+        System::coursereg();        
+      }
+     if(choice==3)
+      {
+                                                //attendance
+      }
+     if(choice ==4)
+      {
+        System::marksassign();
+      }
+     if(choice ==5)
+      {
+        System::coursewith();
+      }
+     if(choice ==6)
+      {
+                                                //exiting
+      }
    }
   void enroll()
   {
-  cout<< "1- Display Already Enrolled Students.\n";
-  cout<< "2- Add a Student.\n";
-  cout<< "3- Remove a Student.\n";
-  cout<< "4- Edit Student Detail.\n";
-  cout<< "5- Exit\n";
-  cout<<"\n\n";
-  cout<<"ENTER YOUR CHOICE:"; 
-  int choice;
-  Validator::input(choice,1,5); 
-  if(choice ==1)
-  {
+    cout<< "1- Display Already Enrolled Students.\n";
+    cout<< "2- Add a Student.\n";
+    cout<< "3- Remove a Student.\n";
+    cout<< "4- Edit Student Detail.\n";
+    cout<< "5- Back\n";
+    cout<<"\n\n";
+    cout<<"ENTER YOUR CHOICE:"; 
+    int choice;
+    Validator::input(choice,1,5); 
+    if(choice ==1)
+     {
 
-  }
-  if(choice==2)
-  {
-   Validator::enroll();
-  }
-  if(choice==3)
-  {
+     }
+    if(choice==2)
+     {
+      Validator::enroll();
+     }
+    if(choice==3)
+     {
 
-  }
-  if(choice ==4)
-  {
+     }
+    if(choice ==4)
+     {
 
+     }
+    if(choice ==5)
+     {
+      System::Menu();
+     } 
   }
-  if(choice ==5)
+  void coursereg()
   {
-   System::Menu();
-  } 
+    cout<< "1- Available Courses.\n";
+    cout<< "2- Registered Courses.\n";
+    cout<< "3- Back\n";
+    cout<<"ENTER YOUR CHOICE:"; 
+    int choice;
+    Validator::input(choice,1,3); 
+    if(choice ==1)
+    {
+
+    }
+    if(choice==2)
+    {
+
+    }
+    if(choice==3)
+    {
+      System::Menu();
+    }
   }
+  void marksassign()
+  {
+    cout<< "1- Available Courses.\n";
+    cout<< "2- Registered Courses.\n";
+    cout<< "3- Back\n";
+    cout<<"ENTER YOUR CHOICE:"; 
+    int choice;
+    Validator::input(choice,1,3); 
+    if(choice ==1)
+     {
+
+     }
+    if(choice==2)
+    {
+
+    }
+   if(choice==3)
+    {
+     System::Menu();
+    }
+  }
+  void coursewith()
+  {
+   cout<< "1- Enrolled Courses.\n";
+   cout<< "2- Drop Course.\n";
+   cout<< "3- Back\n";
+   cout<<"ENTER YOUR CHOICE:"; 
+   int choice;
+   Validator::input(choice,1,3); 
+   if(choice ==1)
+    {
+
+    }
+   if(choice==2)
+    {
+
+    }
+   if(choice==3)
+    {
+     System::Menu();
+    }   
+  }
+
+
+
+
+
+
    ~System(){ }
 };
 class Filehandler
